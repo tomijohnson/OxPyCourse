23833a1924371fab349c520b9cc128659dfc7ace
tomi <savetomi@gmail.com>
2015-05-11 17:48:02 +0100

    Working but blank

--- main.py
+++ main.py
@@ -11,11 +11,36 @@ def templateFunction(inputs):
     """
     return 0
 
+def assignment2():
+    """ Demonstrates the completion of Assignment 2
+    :param None:
+    :return None:
+    """
+    print "--- Starting Assignment 2 ---"
+    alist = [0, -1, 9, 34, -45, 8, 12, 3, 9, 21.9]
+    print "List of numbers:", alist
+    print "Min function returns:", oxpy.getMin(alist), "Expecting:", -45
+    print "Max function returns:", oxpy.getMax(alist), "Expecting:", 34
+    print "--- Finished Assignment 2 ---"
+
+def assignment3():
+    """ Demonstrates the completion of Assignment 3
+    :param None:
+    :return None:
+    """
+    print "--- Starting Assignment 3 ---"
+    alist = [0, -1, 9, 34, -45, 8, 12, 3, 9, 21.9]
+    print "List of numbers:", alist
+    print "Sort function returns:", oxpy.sortList(alist), "Expecting", [ -45,-1, 0, 3, 8, 9, 9, 12, 21.9, 34]
+    print "Min function returns:", oxpy.getMin(alist), "Expecting:", -45
+    print "Max function returns:", oxpy.getMax(alist), "Expecting:", 34
+    print "--- Finished Assignment 3 ---"
+
 # Classes
 
 # Main function
 def main():
-    templateFunction(0)
-    oxpy.getMax(0)
+    assignment2()
+    assignment3()
 
 main()
